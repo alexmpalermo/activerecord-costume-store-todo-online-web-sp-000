8108479c40d43ff25d2556c4bb27b4b78b4d52ec
Alexandra Palermo <alexmpalermo@gmail.com>
2019-03-01 19:28:38 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,7 +6,7 @@ def change
     t.string :location
     t.string :theme
     t.float :price
-    t.integer :family_friendly
+    t.boolean :family_friendly
     t.datetime :opening_date
     t.datetime :closing_date
     t.text :description
